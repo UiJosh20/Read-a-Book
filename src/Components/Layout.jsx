b70641156e06bb6132b664e1d5f4395d6f025a57
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-02-12 18:14:35 +0100

    adjusts files

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Navigation from "./Navigation"
 
 
 const Layout = () => {
   return (
     <main>
+      <Navigation/>
       <Outlet/>
     </main>
   )
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,13 @@
+
+
+const Navigation = () => {
+  return (
+    <main>
+      <nav>
+        <h1>LOGO</h1>
+      </nav>
+    </main>
+  )
+}
+
+export default Navigation
